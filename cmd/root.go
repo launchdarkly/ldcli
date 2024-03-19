7751b734eb11e9264b8a8b6067d7567f8d32739c
Danny Olson <dolson@launchdarkly.com>
2024-03-19 11:34:30 -0700

    Remove init()

--- cmd/root.go
+++ cmd/root.go
@@ -13,20 +13,63 @@ import (
 	"ld-cli/cmd/projects"
 )
 
-var rootCmd = &cobra.Command{
-	Use:     "ldcli",
-	Short:   "LaunchDarkly CLI",
-	Long:    "LaunchDarkly CLI to control your feature flags",
-	Version: "0.0.1", // TODO: set this based on release or use `cmd.SetVersionTemplate(s string)`
+func newRootCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "ldcli",
+		Short:   "LaunchDarkly CLI",
+		Long:    "LaunchDarkly CLI to control your feature flags",
+		Version: "0.0.1", // TODO: set this based on release or use `cmd.SetVersionTemplate(s string)`
 
-	// Handle errors differently based on type.
-	// We don't want to show the usage if the user has the right structure but invalid data such as
-	// the wrong key.
-	SilenceUsage:  true,
-	SilenceErrors: true,
+		// Handle errors differently based on type.
+		// We don't want to show the usage if the user has the right structure but invalid data such as
+		// the wrong key.
+		SilenceUsage:  true,
+		SilenceErrors: true,
+	}
+
+	var (
+		accessToken string
+		baseURI     string
+	)
+
+	cmd.PersistentFlags().StringVarP(
+		&accessToken,
+		"accessToken",
+		"t",
+		"",
+		"LaunchDarkly personal access token",
+	)
+	err := cmd.MarkPersistentFlagRequired("accessToken")
+	if err != nil {
+		panic(err)
+	}
+	err = viper.BindPFlag("accessToken", cmd.PersistentFlags().Lookup("accessToken"))
+	if err != nil {
+		panic(err)
+	}
+
+	cmd.PersistentFlags().StringVarP(
+		&baseURI,
+		"baseUri",
+		"u",
+		"https://app.launchdarkly.com",
+		"LaunchDarkly base URI",
+	)
+	err = viper.BindPFlag("baseUri", cmd.PersistentFlags().Lookup("baseUri"))
+	if err != nil {
+		panic(err)
+	}
+
+	cmd.SetErrPrefix("")
+
+	cmd.AddCommand(projects.NewProjectsCmd())
+	cmd.AddCommand(setupCmd)
+
+	return cmd
 }
 
 func Execute() {
+	rootCmd := newRootCommand()
 	err := rootCmd.Execute()
 	if err != nil {
 		switch {
@@ -39,44 +82,4 @@ func Execute() {
 			fmt.Println(rootCmd.UsageString())
 		}
 	}
-}
-
-func init() {
-	var (
-		accessToken string
-		baseURI     string
-	)
-
-	rootCmd.PersistentFlags().StringVarP(
-		&accessToken,
-		"accessToken",
-		"t",
-		"",
-		"LaunchDarkly personal access token",
-	)
-	err := rootCmd.MarkPersistentFlagRequired("accessToken")
-	if err != nil {
-		panic(err)
-	}
-	err = viper.BindPFlag("accessToken", rootCmd.PersistentFlags().Lookup("accessToken"))
-	if err != nil {
-		panic(err)
-	}
-
-	rootCmd.PersistentFlags().StringVarP(
-		&baseURI,
-		"baseUri",
-		"u",
-		"https://app.launchdarkly.com",
-		"LaunchDarkly base URI",
-	)
-	err = viper.BindPFlag("baseUri", rootCmd.PersistentFlags().Lookup("baseUri"))
-	if err != nil {
-		panic(err)
-	}
-
-	rootCmd.SetErrPrefix("")
-
-	rootCmd.AddCommand(projects.NewProjectsCmd())
-	rootCmd.AddCommand(setupCmd)
 }
