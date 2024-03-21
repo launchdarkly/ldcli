41a6ded744cf67b3a09bbac2adeb686a8957e8db
Danny Olson <dolson@launchdarkly.com>
2024-03-21 11:03:01 -0700

    Fix build

--- internal/setup/flags.go
+++ internal/setup/flags.go
@@ -7,6 +7,7 @@ import (
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 var (
