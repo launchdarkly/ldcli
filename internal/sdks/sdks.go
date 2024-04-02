2a2530f39f2075aca60bb45237ed6cc41be4d3aa
Danny Olson <dolson@launchdarkly.com>
2024-04-02 11:55:05 -0700

    Can replace SDK key in SDK instructions

--- internal/sdks/sdks.go
+++ internal/sdks/sdks.go
@@ -22,3 +22,22 @@ func ReplaceFlagKey(instructions string, key string) string {
 
 	return r.Replace(instructions)
 }
+
+// ReplaceSDKKey changes the placeholder flag key in the SDK instructions to the flag key from
+// the user.
+func ReplaceSDKKey(instructions string, key string) string {
+	r := strings.NewReplacer(
+		"1234567890abcdef",
+		key,
+		"myClientSideID",
+		key,
+		"mobile-key-from-launch-darkly-website",
+		key,
+		"YOUR_SDK_KEY",
+		key,
+		"Your LaunchDarkly SDK key",
+		key,
+	)
+
+	return r.Replace(instructions)
+}
