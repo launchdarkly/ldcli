af6f4f9160ab26323291c39e327141d14787a334
Danny Olson <dolson@launchdarkly.com>
2024-04-10 14:40:35 -0700

    WIP

---
+++ scripts/package-name.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Given a path get the name of the package.
+# ./scripts/package-name.sh packages/sdk/server-node
+# Produces something like:
+# @launchdarkly/node-server-sdk
+
+set -e
+
+node -p "require('./$1/package.json').name";
