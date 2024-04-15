9e7a648d51002b1dbc80d619e2d6fdccf82fe566
Danny Olson <dolson@launchdarkly.com>
2024-04-15 13:48:25 -0700

    Clean up npm release

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-yarn workspace $WORKSPACE pack
 if $LD_RELEASE_IS_DRYRUN ; then
-  # Dry run just pack the workspace.
   echo "Doing a dry run of publishing."
 else
   if $LD_RELEASE_IS_PRERELEASE ; then
     echo "Publishing with prerelease tag."
-    npm publish --tag prerelease --provenance --access public "./$WORKSPACE_PATH/package.tgz" || { echo "npm publish failed" >&2; exit 1; }
+    npm publish --tag prerelease --provenance --access public || { echo "npm publish failed" >&2; exit 1; }
   else
-    npm publish --provenance --access public "./$WORKSPACE_PATH/package.tgz" || { echo "npm publish failed" >&2; exit 1; }
+    npm publish --provenance --access public || { echo "npm publish failed" >&2; exit 1; }
   fi
 fi
