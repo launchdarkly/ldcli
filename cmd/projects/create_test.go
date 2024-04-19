05e9e6f11f9dbf4f4a91c795b8e4fed688426910
Sunny Guduru <sguduru@launchdarkly.com>
2024-04-19 16:48:20 -0700

    added tests for projects

--- cmd/projects/create_test.go
+++ cmd/projects/create_test.go
@@ -147,4 +147,40 @@ func TestCreate(t *testing.T) {
 
 		assert.EqualError(t, err, "base-uri is invalid"+errorHelp)
 	})
+
+	t.Run("will track analytics for 'CLI Command Run' event", func(t *testing.T) {
+		id := "test-id"
+		mockedTrackingArgs := []interface{}{
+			"testAccessToken",
+			"http://test.com",
+			"CLI Command Run",
+			map[string]interface{}{
+				"name":    "projects",
+				"action":  "create",
+				"baseURI": "http://test.com",
+				"id":      id,
+				"flags":   []string{"access-token", "base-uri", "data"},
+			},
+		}
+		tracker := analytics.MockTracker{ID: id}
+		tracker.On("SendEvent", mockedTrackingArgs...)
+
+		client := projects.MockClient{}
+		client.
+			On("Create", mockArgs...).
+			Return([]byte(cmd.ValidResponse), nil)
+		clients := cmd.APIClients{
+			ProjectsClient: &client,
+		}
+		args := []string{
+			"projects", "create",
+			"--access-token", "testAccessToken",
+			"--base-uri", "http://test.com",
+			"-d", `{"key": "test-key", "name": "test-name"}`,
+		}
+
+		_, err := cmd.CallCmd(t, clients, &tracker, args)
+		tracker.AssertCalled(t, "SendEvent", mockedTrackingArgs...)
+		require.NoError(t, err)
+	})
 }
--- cmd/projects/list_test.go
+++ cmd/projects/list_test.go
@@ -120,4 +120,39 @@ func TestList(t *testing.T) {
 
 		assert.EqualError(t, err, "base-uri is invalid"+errorHelp)
 	})
+
+	t.Run("will track analytics for 'CLI Command Run' event", func(t *testing.T) {
+		id := "test-id"
+		mockedTrackingArgs := []interface{}{
+			"testAccessToken",
+			"http://test.com",
+			"CLI Command Run",
+			map[string]interface{}{
+				"name":    "projects",
+				"action":  "list",
+				"baseURI": "http://test.com",
+				"id":      id,
+				"flags":   []string{"access-token", "base-uri"},
+			},
+		}
+		tracker := analytics.MockTracker{ID: id}
+		tracker.On("SendEvent", mockedTrackingArgs...)
+
+		client := projects.MockClient{}
+		client.
+			On("List", mockArgs...).
+			Return([]byte(cmd.ValidResponse), nil)
+		clients := cmd.APIClients{
+			ProjectsClient: &client,
+		}
+		args := []string{
+			"projects", "list",
+			"--access-token", "testAccessToken",
+			"--base-uri", "http://test.com",
+		}
+
+		_, err := cmd.CallCmd(t, clients, &tracker, args)
+		tracker.AssertCalled(t, "SendEvent", mockedTrackingArgs...)
+		require.NoError(t, err)
+	})
 }
