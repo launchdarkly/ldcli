b2abe94f4216f69ca00d21a5f2fc0e1533422873
Sunny Guduru <sguduru@launchdarkly.com>
2024-04-22 10:29:46 -0700

    updated members

--- cmd/members/create_test.go
+++ cmd/members/create_test.go
@@ -122,21 +122,14 @@ func TestCreate(t *testing.T) {
 	})
 
 	t.Run("will track analytics for 'CLI Command Run' event", func(t *testing.T) {
-		id := "test-id"
-		mockedTrackingArgs := []interface{}{
-			"testAccessToken",
-			"http://test.com",
-			"CLI Command Run",
-			map[string]interface{}{
-				"name":    "members",
-				"action":  "create",
-				"baseURI": "http://test.com",
-				"id":      id,
-				"flags":   []string{"access-token", "base-uri", "data"},
-			},
-		}
-		tracker := analytics.MockTracker{ID: id}
-		tracker.On("SendEvent", mockedTrackingArgs...)
+		tracker, mockedTrackingArgs := analytics.MockedTracker(
+			"members",
+			"create",
+			[]string{
+				"access-token",
+				"base-uri",
+				"data",
+			})
 
 		client := members.MockClient{}
 		client.
@@ -154,7 +147,7 @@ func TestCreate(t *testing.T) {
 			`[{"email": "testemail@test.com", "role": "writer"}]`,
 		}
 
-		_, err := cmd.CallCmd(t, clients, &tracker, args)
+		_, err := cmd.CallCmd(t, clients, tracker, args)
 		tracker.AssertCalled(t, "SendEvent", mockedTrackingArgs...)
 		require.NoError(t, err)
 	})
--- cmd/members/invite_test.go
+++ cmd/members/invite_test.go
@@ -229,21 +229,15 @@ func TestInviteWithOptionalRole(t *testing.T) {
 	})
 
 	t.Run("will track analytics for 'CLI Command Run' event", func(t *testing.T) {
-		id := "test-id"
-		mockedTrackingArgs := []interface{}{
-			"testAccessToken",
-			"http://test.com",
-			"CLI Command Run",
-			map[string]interface{}{
-				"name":    "members",
-				"action":  "invite",
-				"baseURI": "http://test.com",
-				"id":      id,
-				"flags":   []string{"access-token", "base-uri", "emails", "role"},
-			},
-		}
-		tracker := analytics.MockTracker{ID: id}
-		tracker.On("SendEvent", mockedTrackingArgs...)
+		tracker, mockedTrackingArgs := analytics.MockedTracker(
+			"members",
+			"invite",
+			[]string{
+				"access-token",
+				"base-uri",
+				"emails",
+				"role",
+			})
 
 		client := members.MockClient{}
 		client.
@@ -260,7 +254,7 @@ func TestInviteWithOptionalRole(t *testing.T) {
 			"--role", "writer",
 		}
 
-		_, err := cmd.CallCmd(t, clients, &tracker, args)
+		_, err := cmd.CallCmd(t, clients, tracker, args)
 		tracker.AssertCalled(t, "SendEvent", mockedTrackingArgs...)
 		require.NoError(t, err)
 	})
