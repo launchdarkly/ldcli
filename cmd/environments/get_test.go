6ac13c3fb06c266521b158ebd25d35a23d4c7176
Sunny Guduru <sguduru@launchdarkly.com>
2024-04-23 12:16:18 -0700

    add test for CLI Completed event

--- cmd/environments/get_test.go
+++ cmd/environments/get_test.go
@@ -144,7 +144,7 @@ func TestGet(t *testing.T) {
 		assert.EqualError(t, err, "base-uri is invalid"+errorHelp)
 	})
 
-	t.Run("will track analytics for CLI Command Run event", func(t *testing.T) {
+	t.Run("will track analytics for CLI Command Run and Completed events", func(t *testing.T) {
 		tracker := analytics.MockedTracker(
 			"environments",
 			"get",
