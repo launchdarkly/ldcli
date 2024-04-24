38c7066e3af38f566fc06d1670270f16d965e50e
Sunny Guduru <sguduru@launchdarkly.com>
2024-04-23 17:51:03 -0700

    remove id from CmdRunEventProperties

--- internal/analytics/events.go
+++ internal/analytics/events.go
@@ -3,14 +3,12 @@ package analytics
 import (
 	"ldcli/cmd/cliflags"
 
-	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
 func CmdRunEventProperties(cmd *cobra.Command, name string) map[string]interface{} {
-	id := uuid.New()
 	baseURI := viper.GetString(cliflags.BaseURIFlag)
 	var flags []string
 	cmd.Flags().Visit(func(f *pflag.Flag) {
@@ -21,7 +19,6 @@ func CmdRunEventProperties(cmd *cobra.Command, name string) map[string]interface
 		"name":   name,
 		"action": cmd.CalledAs(),
 		"flags":  flags,
-		"id":     id.String(),
 	}
 	if baseURI != cliflags.BaseURIDefault {
 		properties["baseURI"] = baseURI
