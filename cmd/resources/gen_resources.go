c48ca3e2a6a40b20d73b67677fd356d1493e3454
Kelly Hofmann <khofmann@launchdarkly.com>
2024-04-30 14:12:09 -0700

    generate template data from openapi spec (teams only)

--- cmd/resources/gen_resources.go
+++ cmd/resources/gen_resources.go
@@ -1,25 +1,30 @@
-package main
+package resources
 
 import (
-	"github.com/getkin/kin-openapi/openapi3"
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/getkin/kin-openapi/openapi3"
+
+	"ldcli/internal/errors"
 )
 
 type TemplateData struct {
-	Resources []ResourceData
+	Resources map[string]*ResourceData
 }
 
 type ResourceData struct {
-	Name       string
-	Operations []OperationData
+	Name        string
+	Description string
+	Operations  []*OperationData
 }
 
 type OperationData struct {
 	Short        string
 	Long         string
 	Use          string
-	Params       []Param
+	Params       []*Param
 	HTTPMethod   string
 	RequiresBody bool
 	Path         string
@@ -35,11 +40,7 @@ type Param struct {
 
 const pathSpecFile = "ld-teams-openapi.json"
 
-func main() {
-	getTemplateData()
-}
-
-func getTemplateData() (TemplateData, error) {
+func GetTemplateData() (TemplateData, error) {
 	rawFile, err := os.ReadFile(pathSpecFile)
 	if err != nil {
 		return TemplateData{}, err
@@ -48,12 +49,80 @@ func getTemplateData() (TemplateData, error) {
 	loader := openapi3.NewLoader()
 	spec, err := loader.LoadFromData(rawFile)
 	if err != nil {
-		return TemplateData{}, nil
+		return TemplateData{}, err
 	}
 
-	for k, v := range spec.Paths.Map() {
-		log.Println(k, ":", v)
+	resources := make(map[string]*ResourceData)
+	for _, r := range spec.Tags {
+		resources[r.Name] = &ResourceData{
+			Name:        r.Name,
+			Description: r.Description,
+			Operations:  make([]*OperationData, 0),
+		}
 	}
 
-	return TemplateData{}, nil
+	for path, pathItem := range spec.Paths.Map() {
+		for method, op := range pathItem.Operations() {
+			// TODO: confirm each op only has one tag
+			tag := op.Tags[0]
+			resource := resources[tag]
+			if resource == nil {
+				return TemplateData{}, errors.NewError(fmt.Sprintf("Matching resource not found for operation's tag: %s", tag))
+			}
+
+			use := methodToCmdUse(method)
+			schema := spec.Components.Schemas[tag]
+			if schema == nil {
+				log.Printf("No schema found for %s", tag)
+			} else {
+				for k, _ := range schema.Value.Properties {
+					if k == "items" {
+						use = "list"
+						break
+					}
+				}
+			}
+
+			operation := OperationData{
+				Short:        op.Summary,
+				Long:         op.Description,
+				Use:          use,
+				Params:       make([]*Param, 0),
+				HTTPMethod:   method,
+				RequiresBody: method == "PUT" || method == "POST" || method == "PATCH",
+				Path:         path,
+			}
+
+			for _, p := range op.Parameters {
+				if p.Value != nil {
+					// TODO: confirm if we only have one type per param b/c somehow this is a slice
+					types := *p.Value.Schema.Value.Type
+					param := Param{
+						Name:        p.Value.Name,
+						In:          p.Value.In,
+						Description: p.Value.Description,
+						Type:        types[0],
+						Required:    p.Value.Required,
+					}
+					operation.Params = append(operation.Params, &param)
+				}
+			}
+
+			resource.Operations = append(resource.Operations, &operation)
+		}
+	}
+
+	return TemplateData{Resources: resources}, nil
+}
+
+func methodToCmdUse(method string) string {
+	methodMap := map[string]string{
+		"GET":    "get",
+		"POST":   "create",
+		"PUT":    "replace", // TODO: confirm this
+		"DELETE": "delete",
+		"PATCH":  "update",
+	}
+
+	return methodMap[method]
 }
