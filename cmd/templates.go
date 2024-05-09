dc9f4a616f2c5a63385ef30674ca4686d8eed453
Kelly Hofmann <khofmann@launchdarkly.com>
2024-05-09 14:12:53 -0700

    docs feedback

--- cmd/templates.go
+++ cmd/templates.go
@@ -5,14 +5,14 @@ func getUsageTemplate() string {
   {{.CommandPath}} [command]
 
 Commands:
-  {{rpad "setup" 29}} Setup guide to create your first feature flag
+  {{rpad "setup" 29}} Create your first feature flag using a step-by-step guide
   {{rpad "config" 29}} View and modify specific configuration values
   {{rpad "completion" 29}} Generate the autocompletion script for the specified shell
 
-Resource Commands:
-  {{rpad "flags" 29}} List, create, modify, and delete feature flags, their statuses, and their expiring targets
-  {{rpad "environments" 29}} Create, delete, and update environments
-  {{rpad "projects" 29}} Create, destroy, and manage projects
+Common resource commands:
+  {{rpad "flags" 29}} List, create, and modify feature flags and their targeting
+  {{rpad "environments" 29}} List, create, and manage environments
+  {{rpad "projects" 29}} List, create, and manage projects
   {{rpad "members" 29}} Invite new members to an account
   {{rpad "segments" 29}} List, create, modify, and delete segments
   {{rpad "..." 29}} To see more resource commands, run 'ldcli resources help'
