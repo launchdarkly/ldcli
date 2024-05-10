41c241f7ed46e5e5baefc3f62bc011afab7150dc
Danny Olson <dolson@launchdarkly.com>
2024-05-10 14:13:30 -0700

    Add new line to end of help output

--- cmd/resources/resources.go
+++ cmd/resources/resources.go
@@ -395,7 +395,8 @@ Global Flags:
 Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
   {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
 
-Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}`
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+`
 }
 
 func operationUsageTemplate() string {
