3f95d920c5cd1c736103699d67dd32223790ab85
Kelly Hofmann <khofmann@launchdarkly.com>
2024-05-13 11:43:29 -0700

    add tests

--- cmd/members/invite.go
+++ cmd/members/invite.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	
+
 	"ldcli/cmd/cliflags"
 	resourcescmd "ldcli/cmd/resources"
 	"ldcli/cmd/validators"
---
+++ cmd/members/invite_test.go
@@ -0,0 +1,49 @@
+package members_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"ldcli/cmd"
+	"ldcli/internal/analytics"
+	"ldcli/internal/resources"
+)
+
+func TestInvite(t *testing.T) {
+	mockClient := &resources.MockClient{
+		Response: []byte(`{
+		   "items":[
+			  {
+				 "_id":"000000000000000000000001",
+				 "role":"writer",
+				 "email":"test1@test.com"
+			  },
+			  {
+				 "_id":"000000000000000000000002",
+				 "role":"writer",
+				 "email":"test2@test.com"
+			  }
+		   ]
+		}`),
+	}
+	args := []string{
+		"members", "invite",
+		"--access-token", "abcd1234",
+		"--emails", "test1@test.com,test2@test.com",
+		"--role", "writer",
+	}
+	output, err := cmd.CallCmd(
+		t,
+		cmd.APIClients{
+			ResourcesClient: mockClient,
+		},
+		analytics.NoopClientFn{}.Tracker(),
+		args,
+	)
+
+	require.NoError(t, err)
+	assert.Equal(t, `[{"email":"test1@test.com","role":"writer"},{"email":"test2@test.com","role":"writer"}]`, string(mockClient.Input))
+	assert.Equal(t, "Successfully updated\n* test1@test.com (000000000000000000000001)\n* test2@test.com (000000000000000000000002)\n", string(output))
+}
