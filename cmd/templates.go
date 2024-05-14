97e8949bc6b4d28ca1a704df9d5adc1673b5f404
Kelly Hofmann <khofmann@launchdarkly.com>
2024-05-14 16:55:42 -0700

    fix resources description too

--- cmd/templates.go
+++ cmd/templates.go
@@ -21,7 +21,7 @@ Common resource commands:
   {{rpad "projects" 29}} List, create, and manage projects
   {{rpad "members" 29}} Invite new members to an account
   {{rpad "segments" 29}} List, create, modify, and delete segments
-  {{rpad "..." 29}} To see more resource commands, run 'ldcli resources help'
+  {{rpad "..." 29}} To see more resource commands, run 'ldcli resources'
 
 Flags:
 {{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}
