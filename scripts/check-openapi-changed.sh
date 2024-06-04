9f66569fea0f1f29784f8d4fb06a4c62b1cd8b0b
Danny Olson <dolson@launchdarkly.com>
2024-06-04 16:09:11 -0700

    Update text

--- scripts/check-openapi-changed.sh
+++ scripts/check-openapi-changed.sh
@@ -2,7 +2,7 @@
 
 DIFF=$(git diff ld-openapi.json)
 if [ "$DIFF" ]; then
-  echo "The OpenAPI spec has been changed. You need to update the generated resources."
+  echo "The OpenAPI spec has been changed. Run 'make openapi-spec-update'."
   exit 1
 else
   echo "The OpenAPI has not changed."
