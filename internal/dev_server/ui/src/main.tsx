a555de4b367d89cf4645334ecf0a595b8998ddb5
Keegan Watkins <109103953+kwatkins-ld@users.noreply.github.com>
2024-06-26 15:34:52 -0500

    feat: add Launchpad and begin to port Gonfalon overrides UI (#339)

--- internal/dev_server/ui/src/main.tsx
+++ internal/dev_server/ui/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { IconProvider } from "./IconProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <IconProvider>
+      <App />
+    </IconProvider>
   </React.StrictMode>,
-)
+);
