a6127c4d8b386d5937fe926bcdb67df3e6bd728f
Keegan Watkins <kwatkins@launchdarkly.com>
2024-06-26 18:16:04 -0500

    feat: Add support for flag overrides from the devserver UI

--- internal/dev_server/ui/src/main.tsx
+++ internal/dev_server/ui/src/main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { IconProvider } from "./IconProvider.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { IconProvider } from './IconProvider.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <IconProvider>
       <App />
