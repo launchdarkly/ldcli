cecb769dafdeecd03046752d74df92c08537ec7f
Keegan Watkins <kwatkins@launchdarkly.com>
2024-06-26 14:32:36 -0500

    feat: add Launchpad and begin to port Gonfalon overrides UI

--- internal/dev_server/ui/src/App.tsx
+++ internal/dev_server/ui/src/App.tsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { LDFlagSet, LDFlagValue } from "launchdarkly-js-client-sdk";
+import { Switch, TextField } from "@launchpad-ui/components";
+import { CopyToClipboard, InlineEdit } from "@launchpad-ui/core";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // TODO: replace these with actual flags when we wire up the APIs
+  const mockFlags: LDFlagSet = {
+    "flag-b": 10,
+    "flag-a": true,
+    "flag-c": {
+      propA: "A",
+    },
+  };
+
+  const sortedFlags = Object.keys(mockFlags)
+    .sort((a, b) => a.localeCompare(b))
+    .reduce<Record<string, LDFlagValue>>((accum, flagKey) => {
+      accum[flagKey] = mockFlags[flagKey];
+
+      return accum;
+    }, {});
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul className="flags-list">
+          {Object.entries(sortedFlags).map(([flagKey, flagValue]) => {
+            let valueNode;
+
+            if (typeof flagValue === "boolean") {
+              valueNode = (
+                <Switch
+                  isSelected={flagValue}
+                  onChange={(newValue) => {
+                    // todo
+                  }}
+                />
+              );
+            } else if (typeof flagValue === "number") {
+              valueNode = (
+                <input
+                  type="number"
+                  value={flagValue}
+                  onChange={(e) => {
+                    // todo
+                  }}
+                />
+              );
+            } else {
+              valueNode = (
+                <InlineEdit
+                  defaultValue={JSON.stringify(flagValue)}
+                  onConfirm={(newValue: string) => {
+                    // todo
+                  }}
+                  renderInput={<TextField id={`${flagKey}-override-input`} />}
+                >
+                  <CopyToClipboard
+                    text={JSON.stringify(flagValue)}
+                    tooltip="Copy flag variation value"
+                  >
+                    {JSON.stringify(flagValue)}
+                  </CopyToClipboard>
+                </InlineEdit>
+              );
+            }
+
+            return (
+              <li key={flagKey}>
+                <span>{flagKey}</span>
+                <div>{valueNode}</div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
