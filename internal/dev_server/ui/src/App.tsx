3f3534e0a0027a247b52b56912771b39bc6c07f7
Keegan Watkins <109103953+kwatkins-ld@users.noreply.github.com>
2024-06-26 17:12:56 -0500

    feat: remove mock flags in devserver UI (#340)
    Remove mock flags in devserver UI

--- internal/dev_server/ui/src/App.tsx
+++ internal/dev_server/ui/src/App.tsx
@@ -1,78 +1,88 @@
 import { LDFlagSet, LDFlagValue } from "launchdarkly-js-client-sdk";
-import { Switch, TextField } from "@launchpad-ui/components";
-import { CopyToClipboard, InlineEdit } from "@launchpad-ui/core";
+import { Switch } from "@launchpad-ui/components";
+import { CopyToClipboard, InlineEdit, TextField } from "@launchpad-ui/core";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  // TODO: replace these with actual flags when we wire up the APIs
-  const mockFlags: LDFlagSet = {
-    "flag-b": 10,
-    "flag-a": true,
-    "flag-c": {
-      propA: "A",
-    },
-  };
+  const [flags, setFlags] = useState<LDFlagSet | null>(null);
 
-  const sortedFlags = Object.keys(mockFlags)
-    .sort((a, b) => a.localeCompare(b))
-    .reduce<Record<string, LDFlagValue>>((accum, flagKey) => {
-      accum[flagKey] = mockFlags[flagKey];
+  useEffect(() => {
+    fetch("/api/dev/projects/default?expand=overrides")
+      .then(async (res) => {
+        if (res.ok) {
+          const flags = (await res.json()).flagsState;
+          const sortedFlags = Object.keys(flags)
+            .sort((a, b) => a.localeCompare(b))
+            .reduce<Record<string, LDFlagValue>>((accum, flagKey) => {
+              accum[flagKey] = flags[flagKey];
 
-      return accum;
-    }, {});
+              return accum;
+            }, {});
+
+          setFlags(sortedFlags);
+        }
+      })
+      .catch((e) => {
+        // todo: handle failure to load flag data
+      });
+  }, []);
 
   return (
     <>
       <div>
         <ul className="flags-list">
-          {Object.entries(sortedFlags).map(([flagKey, flagValue]) => {
-            let valueNode;
+          {flags &&
+            Object.entries(flags).map(([flagKey, { value: flagValue }]) => {
+              let valueNode;
 
-            if (typeof flagValue === "boolean") {
-              valueNode = (
-                <Switch
-                  isSelected={flagValue}
-                  onChange={(newValue) => {
-                    // todo
-                  }}
-                />
-              );
-            } else if (typeof flagValue === "number") {
-              valueNode = (
-                <input
-                  type="number"
-                  value={flagValue}
-                  onChange={(e) => {
-                    // todo
-                  }}
-                />
-              );
-            } else {
-              valueNode = (
-                <InlineEdit
-                  defaultValue={JSON.stringify(flagValue)}
-                  onConfirm={(newValue: string) => {
-                    // todo
-                  }}
-                  renderInput={<TextField id={`${flagKey}-override-input`} />}
-                >
-                  <CopyToClipboard
-                    text={JSON.stringify(flagValue)}
-                    tooltip="Copy flag variation value"
+              if (typeof flagValue === "boolean") {
+                valueNode = (
+                  <Switch
+                    isSelected={flagValue}
+                    onChange={(newValue) => {
+                      // todo
+                    }}
+                  />
+                );
+              } else if (typeof flagValue === "number") {
+                valueNode = (
+                  <input
+                    type="number"
+                    value={flagValue}
+                    onChange={(e) => {
+                      // todo
+                    }}
+                  />
+                );
+              } else {
+                valueNode = (
+                  <InlineEdit
+                    defaultValue={JSON.stringify(flagValue)}
+                    onConfirm={(newValue: string) => {
+                      // todo
+                    }}
+                    renderInput={<TextField id={`${flagKey}-override-input`} />}
                   >
-                    {JSON.stringify(flagValue)}
-                  </CopyToClipboard>
-                </InlineEdit>
-              );
-            }
+                    <CopyToClipboard
+                      text={JSON.stringify(flagValue)}
+                      tooltip="Copy flag variation value"
+                    >
+                      {JSON.stringify(flagValue)}
+                    </CopyToClipboard>
+                  </InlineEdit>
+                );
+              }
 
-            return (
-              <li key={flagKey}>
-                <span>{flagKey}</span>
-                <div>{valueNode}</div>
-              </li>
-            );
-          })}
+              return (
+                <li key={flagKey}>
+                  <span>
+                    <code>{flagKey}</code>
+                  </span>
+                  <div>{valueNode}</div>
+                </li>
+              );
+            })}
         </ul>
       </div>
     </>
--- internal/dev_server/ui/vite.config.ts
+++ internal/dev_server/ui/vite.config.ts
@@ -5,4 +5,13 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8765",
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
