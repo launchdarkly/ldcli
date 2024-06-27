7273f8d58e51b57d25692e0f8d452cc4be745554
Mike Zorn <mike@launchdarkly.com>
2024-06-27 10:05:00 -0700

    feat: Move devserver db to XDG_STATE_HOME (#344)
    * Move devserver db to XDG_STATE_HOME
    
    * Move devserver db to XDG_STATE_HOME
    
    * Use XDG lib instead

--- internal/dev_server/dev_server.go
+++ internal/dev_server/dev_server.go
@@ -7,15 +7,15 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/adrg/xdg"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/launchdarkly/ldcli/internal/dev_server/sdk"
-
 	"github.com/launchdarkly/ldcli/internal/client"
 	"github.com/launchdarkly/ldcli/internal/dev_server/adapters"
 	"github.com/launchdarkly/ldcli/internal/dev_server/api"
 	"github.com/launchdarkly/ldcli/internal/dev_server/db"
 	"github.com/launchdarkly/ldcli/internal/dev_server/model"
+	"github.com/launchdarkly/ldcli/internal/dev_server/sdk"
 )
 
 type Client interface {
@@ -34,7 +34,9 @@ func NewClient(cliVersion string) LDClient {
 
 func (c LDClient) RunServer(ctx context.Context, accessToken, baseURI string) {
 	ldClient := client.New(accessToken, baseURI, c.cliVersion)
-	sqlStore, err := db.NewSqlite(ctx, "devserver.db")
+	dbPath := getDBPath()
+	log.Printf("Using database at %s", dbPath)
+	sqlStore, err := db.NewSqlite(ctx, getDBPath())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -66,4 +68,12 @@ func ResponseErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
 func RequestErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
 	log.Printf("Error while serving request: %+v", err)
 	http.Error(w, err.Error(), http.StatusBadRequest)
+}
+
+func getDBPath() string {
+	dbFilePath, err := xdg.StateFile("ldcli/dev_server.db")
+	if err != nil {
+		log.Fatalf("Unable to create state directory: %s", err)
+	}
+	return dbFilePath
 }
