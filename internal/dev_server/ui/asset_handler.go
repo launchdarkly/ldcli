5808b4ce87364f488a145e57f72f9cfc61c57724
Mike Zorn <mike@launchdarkly.com>
2024-06-27 13:50:56 -0700

    tweak fs so that the paths work out

--- internal/dev_server/ui/asset_handler.go
+++ internal/dev_server/ui/asset_handler.go
@@ -2,10 +2,18 @@ package ui
 
 import (
 	"embed"
+	"io/fs"
+	"log"
 	"net/http"
 )
 
 //go:embed dist/index.html
 var content embed.FS
 
-var AssetHandler = http.FileServerFS(content)
+var AssetHandler = func() http.Handler {
+	dist, err := fs.Sub(content, "dist")
+	if err != nil {
+		log.Fatalf("unable to open dist: %+v", err)
+	}
+	return http.FileServerFS(dist)
+}()
