ad88cfab02c7cc49109a600a86a7af208bd94468
Danny Olson <dbolson@gmail.com>
2024-07-02 12:59:54 -0700

    chore: Simplify config (#361)
    Use config struct type for all config actions

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,20 +16,17 @@ import (
 
 const Filename = ".ldcli-config.yml"
 
-// ConfigFile represents the data stored in the config file.
-type ConfigFile struct {
+type ReadFile func(name string) ([]byte, error)
+
+// Config represents the data stored in the config file.
+type Config struct {
 	AccessToken     string `json:"access-token,omitempty" yaml:"access-token,omitempty"`
 	AnalyticsOptOut *bool  `json:"analytics-opt-out,omitempty" yaml:"analytics-opt-out,omitempty"`
 	BaseURI         string `json:"base-uri,omitempty" yaml:"base-uri,omitempty"`
-	Flag            string `json:"flag,omitempty" yaml:"flag,omitempty"`
 	Environment     string `json:"environment,omitempty" yaml:"environment,omitempty"`
+	Flag            string `json:"flag,omitempty" yaml:"flag,omitempty"`
 	Output          string `json:"output,omitempty" yaml:"output,omitempty"`
 	Project         string `json:"project,omitempty" yaml:"project,omitempty"`
-}
-
-type Config struct {
-	RawConfig map[string]interface{}
-	filename  string
 }
 
 func New(filename string, readFile ReadFile) (Config, error) {
@@ -38,32 +35,17 @@ func New(filename string, readFile ReadFile) (Config, error) {
 		return Config{}, err
 	}
 
-	rawConfig := make(map[string]interface{}, 0)
-	err = yaml.Unmarshal([]byte(data), &rawConfig)
+	var c Config
+	err = yaml.Unmarshal([]byte(data), &c)
 	if err != nil {
-		return Config{}, err
+		return Config{}, errors.NewError("config file is invalid yaml")
 	}
 
-	return Config{
-		RawConfig: rawConfig,
-		filename:  filename,
-	}, nil
+	return c, nil
 }
 
-func (c Config) GetString(k string) string {
-	val, ok := c.RawConfig[k]
-	if !ok {
-		return ""
-	}
-
-	toStr, ok := val.(string)
-	if !ok {
-		return ""
-	}
-
-	return toStr
-}
-
+// Update validates the updating fields and sets them on the Config. It returns the updated fields
+// in addition to the Config.
 func (c Config) Update(kvs []string) (Config, []string, error) {
 	updatedFields := make([]string, 0, len(kvs))
 
@@ -78,84 +60,52 @@ func (c Config) Update(kvs []string) (Config, []string, error) {
 		}
 	}
 
-	for i, a := range kvs {
+	var currField string
+	for i, v := range kvs {
 		if i%2 == 0 {
-			c.RawConfig[a] = struct{}{}
-			updatedFields = append(updatedFields, a)
+			currField = v
+			updatedFields = append(updatedFields, v)
 		} else {
-			c.RawConfig[kvs[i-1]] = a
+			switch currField {
+			case cliflags.AccessTokenFlag:
+				c.AccessToken = v
+			case cliflags.AnalyticsOptOut:
+				val, err := strconv.ParseBool(v)
+				if err != nil {
+					return Config{}, nil, errors.NewError("analytics-opt-out must be true or false")
+				}
+
+				c.AnalyticsOptOut = &val
+			case cliflags.BaseURIFlag:
+				c.BaseURI = v
+			case cliflags.EnvironmentFlag:
+				c.Environment = v
+			case cliflags.FlagFlag:
+				c.Flag = v
+			case cliflags.OutputFlag:
+				val, err := output.NewOutputKind(v)
+				if err != nil {
+					return Config{}, nil, err
+				}
+				c.Output = val.String()
+			case cliflags.ProjectFlag:
+				c.Project = v
+			}
 		}
 	}
 
 	return c, updatedFields, nil
 }
 
-type ReadFile func(name string) ([]byte, error)
-
-func NewConfigFromFile(f string, readFile ReadFile) (ConfigFile, error) {
-	data, err := readFile(f)
-	if err != nil {
-		return ConfigFile{}, errors.NewError("could not read config file")
-	}
-
-	var c ConfigFile
-	err = yaml.Unmarshal([]byte(data), &c)
-	if err != nil {
-		return ConfigFile{}, errors.NewError("config file is invalid yaml")
+// Remove validates the key exists but doesn't do anything else since we unset the value when
+// writing to disk.
+func (c Config) Remove(key string) (Config, error) {
+	_, ok := cliflags.AllFlagsHelp()[key]
+	if !ok {
+		return Config{}, errors.NewError(fmt.Sprintf("%s is not a valid configuration option", key))
 	}
 
 	return c, nil
-}
-
-func NewConfig(rawConfig map[string]interface{}) (ConfigFile, error) {
-	var (
-		accessToken     string
-		analyticsOptOut bool
-		baseURI         string
-		environment     string
-		err             error
-		flag            string
-		outputKind      output.OutputKind
-		project         string
-	)
-	if rawConfig[cliflags.AccessTokenFlag] != nil {
-		accessToken = rawConfig[cliflags.AccessTokenFlag].(string)
-	}
-	if rawConfig[cliflags.AnalyticsOptOut] != nil {
-		stringValue := fmt.Sprintf("%v", rawConfig[cliflags.AnalyticsOptOut])
-		analyticsOptOut, err = strconv.ParseBool(stringValue)
-		if err != nil {
-			return ConfigFile{}, errors.NewError("analytics-opt-out must be true or false")
-		}
-	}
-	if rawConfig[cliflags.BaseURIFlag] != nil {
-		baseURI = rawConfig[cliflags.BaseURIFlag].(string)
-	}
-	if rawConfig[cliflags.EnvironmentFlag] != nil {
-		environment = rawConfig[cliflags.EnvironmentFlag].(string)
-	}
-	if rawConfig[cliflags.FlagFlag] != nil {
-		flag = rawConfig[cliflags.FlagFlag].(string)
-	}
-	if rawConfig[cliflags.OutputFlag] != nil {
-		outputKind, err = output.NewOutputKind(rawConfig[cliflags.OutputFlag].(string))
-		if err != nil {
-			return ConfigFile{}, err
-		}
-	}
-	if rawConfig[cliflags.ProjectFlag] != nil {
-		project = rawConfig[cliflags.ProjectFlag].(string)
-	}
-
-	return ConfigFile{
-		AccessToken:     accessToken,
-		AnalyticsOptOut: &analyticsOptOut,
-		BaseURI:         baseURI,
-		Environment:     environment,
-		Flag:            flag,
-		Output:          outputKind.String(),
-		Project:         project,
-	}, nil
 }
 
 // GetConfigFile gets the full path to the config file.
@@ -174,7 +124,7 @@ func GetConfigFile() string {
 }
 
 func AccessTokenIsSet(filename string) (bool, error) {
-	config, err := NewConfigFromFile(filename, os.ReadFile)
+	config, err := New(filename, os.ReadFile)
 	if err != nil {
 		return false, err
 	}
@@ -183,46 +133,4 @@ func AccessTokenIsSet(filename string) (bool, error) {
 	}
 
 	return true, nil
-}
-
-func Update(kvs []string, filename string) (ConfigFile, error) {
-	if len(kvs)%2 != 0 {
-		return ConfigFile{}, errors.NewError("flag needs an argument: --set")
-	}
-	for i := 0; i < len(kvs)-1; i += 2 {
-		_, ok := cliflags.AllFlagsHelp()[kvs[i]]
-		if !ok {
-			return ConfigFile{}, errors.NewError(fmt.Sprintf("%s is not a valid configuration option", kvs[i]))
-		}
-	}
-
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		return ConfigFile{}, err
-	}
-	rawConfig := make(map[string]interface{}, 0)
-	err = yaml.Unmarshal([]byte(data), &rawConfig)
-	if err != nil {
-		return ConfigFile{}, err
-	}
-
-	// add arg pairs to config where each argument is --set arg1 val1 --set arg2 val2
-	for i, a := range kvs {
-		if i%2 == 0 {
-			rawConfig[a] = struct{}{}
-		} else {
-			rawConfig[kvs[i-1]] = a
-		}
-	}
-
-	for key, value := range rawConfig {
-		rawConfig[key] = value
-	}
-
-	configFile, err := NewConfig(rawConfig)
-	if err != nil {
-		return ConfigFile{}, err
-	}
-
-	return configFile, nil
 }
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -2,7 +2,6 @@ package config_test
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 	"testing"
 
@@ -15,179 +14,16 @@ import (
 )
 
 type mockReadFile struct {
-	contents   []byte
-	successful bool
+	contents []byte
 }
 
 func (m mockReadFile) readFile(name string) ([]byte, error) {
-	if !m.successful {
-		return nil, errors.New("an error")
-	}
-
 	if m.contents != nil {
-		return yaml.Marshal(m.contents)
+		return m.contents, nil
 	}
 
 	return yaml.Marshal(map[string]interface{}{
 		"access-token": "test-access-token",
-	})
-}
-
-func TestNewConfigFromFile(t *testing.T) {
-	t.Run("with valid input is a valid config", func(t *testing.T) {
-		mock := mockReadFile{
-			successful: true,
-		}
-
-		c, err := config.NewConfigFromFile("test-config-file", mock.readFile)
-
-		require.NoError(t, err)
-		assert.Equal(t, "test-access-token", c.AccessToken)
-	})
-
-	t.Run("with invalid file is an error", func(t *testing.T) {
-		mock := mockReadFile{
-			successful: false,
-		}
-
-		_, err := config.NewConfigFromFile("test-config-file", mock.readFile)
-
-		assert.EqualError(t, err, "could not read config file")
-	})
-
-	t.Run("with invalid formatting in the file contents is an error", func(t *testing.T) {
-		mock := mockReadFile{
-			contents:   []byte(`invalid`),
-			successful: true,
-		}
-
-		_, err := config.NewConfigFromFile("test-config-file", mock.readFile)
-
-		assert.EqualError(t, err, "config file is invalid yaml")
-	})
-}
-
-func TestNewConfig(t *testing.T) {
-	t.Run("analytics-opt-out", func(t *testing.T) {
-		tests := map[string]struct {
-			expected bool
-			input    interface{}
-		}{
-			"when value is true": {
-				expected: true,
-				input:    true,
-			},
-			"when value is 1": {
-				expected: true,
-				input:    1,
-			},
-			"when value is false": {
-				expected: false,
-				input:    false,
-			},
-			"when value is 0": {
-				expected: false,
-				input:    0,
-			},
-		}
-		for name, tt := range tests {
-			tt := tt
-			t.Run(fmt.Sprintf("%s is %t", name, tt.expected), func(t *testing.T) {
-				rawConfig := map[string]interface{}{
-					"analytics-opt-out": tt.input,
-				}
-
-				configFile, err := config.NewConfig(rawConfig)
-
-				require.NoError(t, err)
-				assert.Equal(t, tt.expected, *configFile.AnalyticsOptOut)
-			})
-		}
-	})
-
-	t.Run("is an error when analytics-opt-out is something else", func(t *testing.T) {
-		rawConfig := map[string]interface{}{
-			"analytics-opt-out": "something",
-		}
-
-		_, err := config.NewConfig(rawConfig)
-
-		assert.EqualError(t, err, "analytics-opt-out must be true or false")
-	})
-
-	t.Run("analytics-opt-out", func(t *testing.T) {
-		tests := map[string]struct {
-			input string
-		}{
-			"is valid when value is json": {
-				input: "json",
-			},
-			"is valid when value is plaintext": {
-				input: "json",
-			},
-		}
-		for name, tt := range tests {
-			tt := tt
-			t.Run(name, func(t *testing.T) {
-				rawConfig := map[string]interface{}{
-					"output": tt.input,
-				}
-
-				configFile, err := config.NewConfig(rawConfig)
-
-				require.NoError(t, err)
-				assert.Equal(t, tt.input, configFile.Output)
-			})
-		}
-	})
-
-	t.Run("is invalid with an invalid output", func(t *testing.T) {
-		rawConfig := map[string]interface{}{
-			"output": "invalid",
-		}
-
-		_, err := config.NewConfig(rawConfig)
-
-		assert.EqualError(t, err, "output is invalid, use 'json' or 'plaintext'")
-	})
-
-	t.Run("environment", func(t *testing.T) {
-		t.Run("sets the given value", func(t *testing.T) {
-			rawConfig := map[string]interface{}{
-				"environment": "test-key",
-			}
-
-			configFile, err := config.NewConfig(rawConfig)
-
-			require.NoError(t, err)
-			assert.Equal(t, "test-key", configFile.Environment)
-		})
-	})
-
-	t.Run("flag", func(t *testing.T) {
-		t.Run("sets the given value", func(t *testing.T) {
-			rawConfig := map[string]interface{}{
-				"flag": "test-key",
-			}
-
-			configFile, err := config.NewConfig(rawConfig)
-
-			require.NoError(t, err)
-			assert.Equal(t, "test-key", configFile.Flag)
-		})
-	})
-
-	t.Run("project", func(t *testing.T) {
-		t.Run("sets the given value", func(t *testing.T) {
-			rawConfig := map[string]interface{}{
-				"project": "test-key",
-			}
-
-			configFile, err := config.NewConfig(rawConfig)
-
-			require.NoError(t, err)
-			assert.Equal(t, "test-key", configFile.Project)
-		})
 	})
 }
 
@@ -209,5 +45,104 @@ func TestService_VerifyAccessToken(t *testing.T) {
 		isValid := service.VerifyAccessToken("invalid-access-token", "http://test.com")
 
 		assert.False(t, isValid)
+	})
+}
+
+func TestNew(t *testing.T) {
+	mock := mockReadFile{
+		contents: []byte(`
+access-token: test-access-token
+analytics-opt-out: true
+`,
+		),
+	}
+
+	c, err := config.New("test-config", mock.readFile)
+
+	require.NoError(t, err)
+	assert.Equal(t, "test-access-token", c.AccessToken)
+	assert.True(t, *c.AnalyticsOptOut)
+}
+
+func TestUpdate(t *testing.T) {
+	c, err := config.New("test", mockReadFile{}.readFile)
+	require.NoError(t, err)
+
+	t.Run("updates valid fields", func(t *testing.T) {
+		result, updatedFields, err := c.Update(
+			[]string{
+				"access-token", "test-access-token",
+				"analytics-opt-out", "true",
+				"base-uri", "http://test.com",
+				"environment", "test-environment",
+				"flag", "test-flag",
+				"output", "plaintext",
+				"project", "test-project",
+			},
+		)
+
+		require.NoError(t, err)
+		assert.Equal(t, "test-access-token", result.AccessToken)
+		assert.True(t, *result.AnalyticsOptOut)
+		assert.Equal(t, "http://test.com", result.BaseURI)
+		assert.Equal(t, "test-environment", result.Environment)
+		assert.Equal(t, "test-flag", result.Flag)
+		assert.Equal(t, "plaintext", result.Output)
+		assert.Equal(t, "test-project", result.Project)
+		assert.Equal(
+			t,
+			[]string{
+				"access-token",
+				"analytics-opt-out",
+				"base-uri",
+				"environment",
+				"flag",
+				"output",
+				"project",
+			},
+			updatedFields,
+		)
+	})
+
+	t.Run("with an invalid output flag", func(t *testing.T) {
+		_, _, err = c.Update([]string{"output", "invalid"})
+
+		assert.EqualError(t, err, "output is invalid. Use 'json' or 'plaintext'")
+	})
+
+	t.Run("with an invalid analytics-opt-out flag", func(t *testing.T) {
+		_, _, err = c.Update([]string{"analytics-opt-out", "invalid"})
+
+		assert.EqualError(t, err, "analytics-opt-out must be true or false")
+	})
+
+	t.Run("with an invalid amount of flags", func(t *testing.T) {
+		_, _, err = c.Update([]string{"access-token"})
+
+		assert.EqualError(t, err, "flag needs an argument: --set")
+	})
+
+	t.Run("with an invalid flag", func(t *testing.T) {
+		_, _, err = c.Update([]string{"invalid-flag", "val"})
+
+		assert.EqualError(t, err, "invalid-flag is not a valid configuration option")
+	})
+}
+
+func TestRemove(t *testing.T) {
+	c, err := config.New("test", mockReadFile{}.readFile)
+	require.NoError(t, err)
+
+	t.Run("with a valid flag is a no-op", func(t *testing.T) {
+		c, err = c.Remove("access-token")
+
+		require.NoError(t, err)
+		assert.Equal(t, "test-access-token", c.AccessToken)
+	})
+
+	t.Run("with an invalid flag is an error", func(t *testing.T) {
+		c, err = c.Remove("invalid")
+
+		assert.EqualError(t, err, "invalid is not a valid configuration option")
 	})
 }
