183c9358f96144274a051afdb5aab551a88b76f6
Mike Zorn <mike@launchdarkly.com>
2024-07-03 16:21:17 -0700

    add failing unit test for deregister issue

--- internal/dev_server/model/observer_test.go
+++ internal/dev_server/model/observer_test.go
@@ -1,8 +1,10 @@
 package model
 
 import (
+	"sync"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -33,5 +35,25 @@ func TestObservers(t *testing.T) {
 		assert.True(t, ok, "observer should be deregistered")
 		observers.Notify("lol")
 	})
-
+	t.Run("deregistering from multiple go routines should not panic", func(t *testing.T) {
+		observers := NewObservers()
+		observer := testObserver{handle: func(i interface{}) {
+			assert.Fail(t, "should not be called")
+		}}
+		ids := make([]uuid.UUID, 100)
+		for i := 0; i < 100; i++ {
+			i := i
+			ids[i] = observers.RegisterObserver(observer)
+		}
+		wg := sync.WaitGroup{}
+		for _, id := range ids {
+			id := id
+			wg.Add(1)
+			go func() {
+				observers.DeregisterObserver(id)
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	})
 }
