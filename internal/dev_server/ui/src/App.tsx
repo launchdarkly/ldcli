6192201877c3843529cc61a3a73798bfb443b5d7
Abram Thau <47679905+Glacian22@users.noreply.github.com>
2024-07-09 13:28:09 -0700

    Abram UI enhancements (#359)
    * JSON flags now use modal, catch formatting errors
    
    * fixed 'only show flags with overrides' bug
    
    * refactoring
    
    * refactoring
    
    * json update modal now dismissed when new override value accepted
    
    * minor tweak
    
    * remove unnecessarily generated /bin, /.vscode, and undo version change to package-lock
    
    * Use close render prop from Dialog
    
    ---------
    
    Co-authored-by: Abram Thau <abramthau@Abrams-Laptop.local>
    Co-authored-by: Keegan Watkins <kwatkins@launchdarkly.com>

--- internal/dev_server/ui/src/App.tsx
+++ internal/dev_server/ui/src/App.tsx
@@ -5,20 +5,19 @@ import {
   IconButton,
   Label,
   Switch,
+  Modal,
+  ModalOverlay,
+  DialogTrigger,
+  Dialog,
 } from '@launchpad-ui/components';
-import {
-  Box,
-  CopyToClipboard,
-  InlineEdit,
-  TextField,
-} from '@launchpad-ui/core';
+import { Box, InlineEdit, TextField } from '@launchpad-ui/core';
 import Theme from '@launchpad-ui/tokens';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Icon } from '@launchpad-ui/icons';
 
 const API_BASE = import.meta.env.PROD ? '' : '/api';
-const apiRoute = (pathname: string) => `${API_BASE}${pathname}`
+const apiRoute = (pathname: string) => `${API_BASE}${pathname}`;
 
 function App() {
   const [flags, setFlags] = useState<LDFlagSet | null>(null);
@@ -28,6 +27,7 @@ function App() {
   > | null>(null);
   const [onlyShowOverrides, setOnlyShowOverrides] = useState(false);
   const overridesPresent = overrides && Object.keys(overrides).length > 0;
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   const updateOverride = (flagKey: string, overrideValue: LDFlagValue) => {
     fetch(apiRoute(`/dev/projects/default/overrides/${flagKey}`), {
@@ -71,6 +71,9 @@ function App() {
           delete updatedOverrides[flagKey];
 
           setOverrides(updatedOverrides);
+
+          if (Object.keys(updatedOverrides).length === 0)
+            setOnlyShowOverrides(false);
         }
       })
       .catch((_e) => {
@@ -225,24 +228,67 @@ function App() {
                 break;
               default:
                 valueNode = (
-                  <InlineEdit
-                    defaultValue={JSON.stringify(
-                      hasOverride ? overrideValue : flagValue,
-                    )}
-                    onConfirm={(newValue: string) => {
-                      updateOverride(flagKey, JSON.parse(newValue));
-                    }}
-                    renderInput={<TextField id={`${flagKey}-override-input`} />}
-                  >
-                    <CopyToClipboard
-                      text={JSON.stringify(
-                        hasOverride ? overrideValue : flagValue,
-                      )}
-                      tooltip="Copy flag variation value"
-                    >
-                      {JSON.stringify(hasOverride ? overrideValue : flagValue)}
-                    </CopyToClipboard>
-                  </InlineEdit>
+                  <DialogTrigger>
+                    <Button style={{ border: 'none', padding: 0, margin: 0 }}>
+                      <textarea
+                        rows={8}
+                        readOnly={true}
+                        style={{
+                          resize: 'none',
+                          overflowY: 'clip',
+                          cursor: 'pointer',
+                        }}
+                        value={JSON.stringify(
+                          hasOverride ? overrideValue : flagValue,
+                          null,
+                          2,
+                        )}
+                      ></textarea>
+                    </Button>
+                    <ModalOverlay>
+                      <Modal>
+                        <Dialog>
+                          {({ close }) => (
+                            <form
+                              onSubmit={() => {
+                                let newVal;
+
+                                try {
+                                  newVal = JSON.parse(
+                                    textAreaRef?.current?.value || '',
+                                  );
+                                } catch (err) {
+                                  window.alert('Invalid JSON formatting');
+                                  return;
+                                }
+
+                                updateOverride(flagKey, newVal);
+                              }}
+                            >
+                              <textarea
+                                ref={textAreaRef}
+                                style={{ width: '100%', height: '30rem' }}
+                                defaultValue={JSON.stringify(
+                                  hasOverride ? overrideValue : flagValue,
+                                  null,
+                                  2,
+                                )}
+                              />
+                              <div>
+                                <Button
+                                  variant="primary"
+                                  type="submit"
+                                  onPress={close}
+                                >
+                                  Accept
+                                </Button>
+                              </div>
+                            </form>
+                          )}
+                        </Dialog>
+                      </Modal>
+                    </ModalOverlay>
+                  </DialogTrigger>
                 );
             }
 
