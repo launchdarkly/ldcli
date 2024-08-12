426c9f17c8ee14733c2bd0bcf7d7cc05d9560a95
Hosh <hoshsadiq@users.noreply.github.com>
2024-08-12 19:06:22 +0000

    fix: add credentials to cors (#384)

--- internal/dev_server/sdk/cors.go
+++ internal/dev_server/sdk/cors.go
@@ -6,6 +6,7 @@ func CorsHeaders(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Set("Access-Control-Allow-Origin", "*")
 		writer.Header().Set("Access-Control-Allow-Methods", "GET,OPTIONS")
+		writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		writer.Header().Set("Access-Control-Allow-Headers", "Cache-Control,Content-Type,Content-Length,Accept-Encoding,X-LaunchDarkly-User-Agent,X-LaunchDarkly-Payload-ID,X-LaunchDarkly-Wrapper,X-LaunchDarkly-Event-Schema,X-LaunchDarkly-Tags")
 		writer.Header().Set("Access-Control-Expose-Headers:", "Date")
 		writer.Header().Set("Access-Control-Max-Age", "300")
@@ -17,6 +18,7 @@ func EventsCorsHeaders(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Set("Access-Control-Allow-Origin", "*")
 		writer.Header().Set("Access-Control-Allow-Methods", "POST,OPTIONS")
+		writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		writer.Header().Set("Access-Control-Allow-Headers", "Accept,Content-Type,Content-Length,Accept-Encoding,X-LaunchDarkly-Event-Schema,X-LaunchDarkly-User-Agent,X-LaunchDarkly-Payload-ID,X-LaunchDarkly-Wrapper,X-LaunchDarkly-Tags")
 		writer.Header().Set("Access-Control-Expose-Headers:", "Date")
 		writer.Header().Set("Access-Control-Max-Age", "300")
