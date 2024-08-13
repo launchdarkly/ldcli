9cf6f09802732d8a88548870abf44b90cf7b85a3
Mike Zorn <mike@launchdarkly.com>
2024-08-13 16:53:00 -0700

    Prettier

--- internal/dev_server/ui/src/App.tsx
+++ internal/dev_server/ui/src/App.tsx
@@ -1,36 +1,40 @@
 import './App.css';
-import {useState} from 'react';
-import Flags from "./Flags.tsx";
-import ProjectSelector from "./ProjectSelector.tsx";
-import {Box} from "@launchpad-ui/core";
+import { useState } from 'react';
+import Flags from './Flags.tsx';
+import ProjectSelector from './ProjectSelector.tsx';
+import { Box } from '@launchpad-ui/core';
 
 function App() {
-  const [selectedProject, setSelectedProject] = useState<string|null >(null);
+  const [selectedProject, setSelectedProject] = useState<string | null>(null);
 
   return (
     <>
       <Box
-          style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems:"center"
-          }}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="flex-start"
+          marginBottom="2rem"
+          padding="1rem"
+          gap="2rem"
         >
-          <Box
-              display="flex"
-              flexDirection="row"
-              justifyContent="space-between"
-              alignItems="flex-start"
-              marginBottom="2rem"
-              padding="1rem"
-              gap="2rem"
-          >
-              <ProjectSelector
-                  selectedProject={selectedProject}
-                  setSelectedProject={setSelectedProject}
-              />
-          </Box>
-          {selectedProject != null ? <Flags selectedProject={selectedProject}/> : "" }
+          <ProjectSelector
+            selectedProject={selectedProject}
+            setSelectedProject={setSelectedProject}
+          />
+        </Box>
+        {selectedProject != null ? (
+          <Flags selectedProject={selectedProject} />
+        ) : (
+          ''
+        )}
       </Box>
     </>
   );
