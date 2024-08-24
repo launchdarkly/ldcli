f2e45750f17dcc65fcfdf79985def386af64b181
Mike Zorn <mike@launchdarkly.com>
2024-08-23 17:48:05 -0700

    Add dev server to usage template

--- cmd/templates.go
+++ cmd/templates.go
@@ -15,6 +15,7 @@ Commands:
   {{rpad "config" 29}} View and modify specific configuration values
   {{rpad "completion" 29}} Enable command autocompletion within supported shells
   {{rpad "login" 29}} Log in to your LaunchDarkly account
+  {{rpad "dev-server" 29}} Run a development server to serve flags locally
 
 Common resource commands:
   {{rpad "flags" 29}} List, create, and modify feature flags and their targeting
