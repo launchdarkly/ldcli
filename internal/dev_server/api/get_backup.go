95d64c900aa20d358ec065039e0acfa9bb76668a
Daniel Mashuda <dmashuda@launchdarkly.com>
2025-02-03 11:42:16 -0500

    Break out backup

---
+++ internal/dev_server/api/get_backup.go
@@ -0,0 +1,8 @@
+package api
+
+import "context"
+
+func (s server) GetBackup(ctx context.Context, request GetBackupRequestObject) (GetBackupResponseObject, error) {
+	//TODO implement me
+	panic("implement me")
+}
--- internal/dev_server/api/server.go
+++ internal/dev_server/api/server.go
@@ -1,16 +1,9 @@
 package api
 
-import "context"
-
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config oapi-codegen-cfg.yaml api.yaml
 var _ StrictServerInterface = server{}
 
 type server struct {
-}
-
-func (s server) GetBackup(ctx context.Context, request GetBackupRequestObject) (GetBackupResponseObject, error) {
-	//TODO implement me
-	panic("implement me")
 }
 
 func NewStrictServer() StrictServerInterface {
