75da6a49af921db7b858ed2fb4115db71627f143
Mike Zorn <mike@launchdarkly.com>
2025-02-19 16:16:05 -0800

    Add eslint config

--- internal/dev_server/ui/eslint.config.js
+++ internal/dev_server/ui/eslint.config.js
@@ -3,12 +3,20 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ignores: ["./src/__mocks__/@launchpad-ui/*.js"],
+  },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
