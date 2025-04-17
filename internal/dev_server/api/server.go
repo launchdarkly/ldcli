f0f9ceea5403d620efd6116c38a35d7736bc4bcc
Daniel Mashuda <dmashuda@launchdarkly.com>
2025-04-17 12:25:48 -0400

    Port go generate to genrule

--- internal/dev_server/api/server.go
+++ internal/dev_server/api/server.go
@@ -1,6 +1,5 @@
 package api
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config oapi-codegen-cfg.yaml api.yaml
 var _ StrictServerInterface = server{}
 
 type server struct {
