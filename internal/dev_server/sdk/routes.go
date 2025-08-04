5c75a46cef0f8af0cc53f55eeba7bc55fbc4dabf
Mori Bellamy <mbellamy@launchdarkly.com>
2025-08-04 14:04:43 -0700

    fix typo in function name

--- internal/dev_server/sdk/routes.go
+++ internal/dev_server/sdk/routes.go
@@ -10,13 +10,13 @@ var DevNull = ConstantResponseHandler(http.StatusAccepted, "")
 
 func BindRoutes(router *mux.Router) {
 	// events
-	router.HandleFunc("/bulk", SdkEventswHandler)
+	router.HandleFunc("/bulk", SdkEventsHandler)
 	router.HandleFunc("/diagnostic", DevNull)
 	router.Handle("/events/bulk/{envId}", EventsCorsHeaders(DevNull))
 	router.Handle("/events/diagnostic/{envId}", EventsCorsHeaders(DevNull))
 	router.HandleFunc("/mobile", DevNull)
 	router.HandleFunc("/mobile/events", DevNull)
-	router.HandleFunc("/mobile/events/bulk", SdkEventswHandler)
+	router.HandleFunc("/mobile/events/bulk", SdkEventsHandler)
 	router.HandleFunc("/mobile/events/diagnostic", DevNull)
 
 	router.Handle("/all", GetProjectKeyFromAuthorizationHeader(http.HandlerFunc(StreamServerAllPayload)))
--- internal/dev_server/sdk/sdk_events.go
+++ internal/dev_server/sdk/sdk_events.go
@@ -6,11 +6,11 @@ import (
 	"net/http"
 )
 
-func SdkEventswHandler(writer http.ResponseWriter, request *http.Request) {
+func SdkEventsHandler(writer http.ResponseWriter, request *http.Request) {
 	log.Println(request.URL.Path)
 	bodyStr, err := io.ReadAll(request.Body)
 	if err != nil {
-		log.Printf("SdkEventswHandler: error reading request body: %v", err)
+		log.Printf("SdkEventsHandler: error reading request body: %v", err)
 		return
 	}
 	log.Println(string(bodyStr))
